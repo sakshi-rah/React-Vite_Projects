8592b1b4e7452181a1e20cdc7503b2a0f79c057b
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2023-10-11 12:30:37 +0530

    feat: password generator completed

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,6 +1,7 @@
 import { useCallback, useState, useEffect, useRef } from "react"
 
 function App() {
+  const passwordRef = useRef(null);
   const [length, setLength] = useState(8);
   const [getnumber, setGetnumber] = useState(false);
   const [character, setCharacter] = useState(false);
@@ -15,38 +16,46 @@ function App() {
 
     for (let i = 1; i <=length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
-      pass = str.charAt(char);
+      pass += str.charAt(char);
     }
     setPassword(pass)
 
   }, [length, getnumber, character, setPassword])
 
+  const copyPasswordToClip = useCallback(()=>{
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0, 50)
+    window.navigator.clipboard.writeText(password)
+  },[password])
+
+  
   useEffect(()=>{
     passwordGenerator()
   },[length, getnumber, character,passwordGenerator])
 
   return (
     <div>
-      <div className="w-full max-w-lg m-auto shadow-md bg-gray-700 text-lime-600 rounded-md px-3 p-3 text-2xl font-mono">
-        <h1 className='text-4xl text-center text-white py-2'>Password Generator</h1>
+      <div className="w-full max-w-lg m-auto shadow-md bg-gray-700 text-lime-500 rounded-md px-5 p-10 text-2xl font-mono">
+        <h1 className='text-4xl text-center text-white'>Password Generator</h1>
 
-        <div className="flex overflow-hidden rounded-md shadow mb-4">
+        <div className="flex overflow-hidden rounded-md shadow my-5">
           <input
             type="text"
             value={password}
             className="outline-none w-full py-1 px-3"
             placeholder="Password"
             readOnly
+            ref={passwordRef}
           />
-          <button className="text-white bg-lime-600 p-2">Copy</button>
+          <button onClick={copyPasswordToClip} className="text-white bg-lime-500 p-2 hover:bg-lime-400">Copy</button>
         </div>
 
         <div className="flex text-xl gap-x-2">
           <div className="flex items-center gap-x-1">
             <input
               type="range"
-              min={6}
-              max={50}
+              min={8}
+              max={30}
               value={length}
               className="cursor-pointer"
               onChange={(e) => { setLength(e.target.value) }}
